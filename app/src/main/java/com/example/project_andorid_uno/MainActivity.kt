7a626de5a6ed46c54862f5f6cd9a8f472a549ba0
BZ <127404282+inf21064@users.noreply.github.com>
2023-05-08 13:31:01 +0200

    Setup

---
+++ app/src/main/java/com/example/project_andorid_uno/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.project_andorid_uno
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
