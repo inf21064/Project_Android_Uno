77ba21d2fa827bd4efd5d92a689ab49d07bd0a41
BZ <127404282+inf21064@users.noreply.github.com>
2023-05-12 15:05:12 +0200

    one can start the app working playbutton and navigationUp

--- app/src/main/java/com/example/project_andorid_uno/MainActivity.kt
+++ app/src/main/java/com/example/project_andorid_uno/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.project_andorid_uno
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.project_andorid_uno.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        navController = findNavController(R.id.myNavHostFragment)
+        setupActionBarWithNavController(navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
