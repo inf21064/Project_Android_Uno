8b5039c1a32e1e2b47e798d63bd305855d7dd6fb
BZ <127404282+inf21064@users.noreply.github.com>
2023-05-30 20:28:49 +0200

    added end Turn Button and logic for draw Card and end Turn button visibility

--- app/src/main/java/com/example/project_andorid_uno/PlayedCards.kt
+++ app/src/main/java/com/example/project_andorid_uno/PlayedCards.kt
@@ -6,14 +6,20 @@ import android.widget.Toast
 import kotlin.random.Random
 import kotlin.system.exitProcess
 
-class PlayedCards(val startCard:PlayingCard, val context: Context?, private val playedCardImageView: ImageView) {
+class PlayedCards(val startCard:PlayingCard, val context: Context?, private val playedCardImageView: ImageView, private val gameFragment: GameFragment) {
 
     val playedCards: MutableList<PlayingCard> = mutableListOf(startCard)
     var whoHasTurn = "Player"
-    var cardDrawn = false
+    private var _cardDrawn = false
     var saidUno = false
     var skipTurns = 0
 
+    var cardDrawn: Boolean
+        get() = _cardDrawn
+        set(value) {
+            gameFragment.changeEndTurnButtonVisibility(value)
+            _cardDrawn = value
+        }
     fun updateImage(imageResId: Int) {
         playedCardImageView.setImageResource(imageResId)
     }
